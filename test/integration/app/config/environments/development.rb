f34ae50c5022bc1a69f082981e6a1d810806906c
Evan Weaver <evan@cloudbur.st>
2007-10-04 22:08:04 +0000

    oops

--- test/integration/app/config/environments/development.rb
+++ test/integration/app/config/environments/development.rb
@@ -1,4 +1,4 @@
-config.cache_classes = true
+config.cache_classes = false
 config.whiny_nils = true
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
