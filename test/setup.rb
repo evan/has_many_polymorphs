339b78062768146d8c704f2f2930730228a1fb74
Evan Weaver <evan@cloudbur.st>
2007-10-04 21:12:44 +0000

    include prod env

--- test/setup.rb
+++ test/setup.rb
@@ -2,8 +2,8 @@
 # Setup integration system for the integration suite
 
 Dir.chdir "#{File.dirname(__FILE__)}/integration/app/" do
-  ENV['RAILS_ENV'] = 'development'
-  system("rake db:create db:migrate db:fixtures:load")
-  ENV['RAILS_ENV'] = 'test'
-  system("rake db:create db:migrate db:fixtures:load")
+  %w(production development test).each do |env|
+    ENV['RAILS_ENV'] = env
+    system("rake db:create db:migrate db:fixtures:load")
+  end
 end
