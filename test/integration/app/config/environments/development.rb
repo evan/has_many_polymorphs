81f9b777073c52c7de53c47be414eec74392b65b
Evan Weaver <evan@cloudbur.st>
2007-10-04 22:07:36 +0000

    keep spelling interface even if spelling didn't load

--- test/integration/app/config/environments/development.rb
+++ test/integration/app/config/environments/development.rb
@@ -1,4 +1,4 @@
-config.cache_classes = false
+config.cache_classes = true
 config.whiny_nils = true
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
