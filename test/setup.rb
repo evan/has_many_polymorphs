7307da0050f75c5520d76cfa518c5739c9a585c6
Evan Weaver <evan@cloudbur.st>
2007-12-15 10:40:40 +0000

    setup rb doesnt run migrations

--- test/setup.rb
+++ test/setup.rb
@@ -5,6 +5,4 @@ Dir.chdir "#{File.dirname(__FILE__)}/integration/app/" do
   Dir.chdir "vendor/plugins" do
     system("rm has_many_polymorphs; ln -s ../../../../../ has_many_polymorphs")
   end
-  system("rake db:create")
-  system("rake db:migrate db:fixtures:load")
 end
