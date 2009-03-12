e3f0db41c941266659989b1ce3569186925abd82
W. Andrew Loe III <andrew@andrewloe.com>
2009-03-13 01:50:55 +0800

    Reverting the rest of the bad table_name -> quoted_table_name changes.
    100% of the tests pass on 2.2.2 against MySQL 5.0.45.
    
    Signed-off-by: Snax Fauna <evan+fauna@cloudbur.st>

--- lib/has_many_polymorphs/base.rb
+++ lib/has_many_polymorphs/base.rb
@@ -16,12 +16,12 @@ module ActiveRecord
           
           # find the real child class
           klass = record["#{self.table_name}.#{reflection.options[:polymorphic_type_key]}"].constantize
-          if sti_klass = record["#{klass.quoted_table_name}.#{klass.inheritance_column}"]
+          if sti_klass = record["#{klass.table_name}.#{klass.inheritance_column}"]
             klass = klass.class_eval do compute_type(sti_klass) end # in case of namespaced STI models
           end
           
           # check that the join actually joined to something
-          unless (child_id = record["#{self.quoted_table_name}.#{reflection.options[:polymorphic_key]}"]) == record["#{klass.quoted_table_name}.#{klass.primary_key}"]
+          unless (child_id = record["#{self.table_name}.#{reflection.options[:polymorphic_key]}"]) == record["#{klass.table_name}.#{klass.primary_key}"]
             raise ActiveRecord::Associations::PolymorphicError, 
               "Referential integrity violation; child <#{klass.name}:#{child_id}> was not found for #{reflection.name.inspect}" 
           end
@@ -29,7 +29,7 @@ module ActiveRecord
           # eject the join keys
           # XXX not very readable
           record = Hash[*record._select do |column, value| 
-            column[/^#{klass.quoted_table_name}/]
+            column[/^#{klass.table_name}/]
           end.map do |column, value|
             [column[/\.(.*)/, 1], value]
           end.flatten]
